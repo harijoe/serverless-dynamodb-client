c12090d0162190c3dc754792ec5a932c248f46ca
Julien Vallini <vallinij@gmail.com>
2018-11-20 01:59:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import { doc as dynamoDb } from 'serverless-dynamodb-client';
+
+class DynamoDB {
+  table = null
+  constructor(table) {
+    this.table = table
+  }
+  get = async (entity, id) => {
+    const { Item } = await dynamoDb
+      .get({
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+          id,
+          entity,
+        },
+      })
+      .promise();
+
+    return Item ? Item.value : null;
+  }
+  put = async data => {
+    if (!data.id) {
+      throw new Error('data provided to dynamoDB.put should contain an id');
+    }
+    if (!data.entity) {
+      throw new Error('data provided to dynamoDB.put should contain a partition');
+    }
+    if (!data.value) {
+      throw new Error('data provided to dynamoDB.put should contain a value');
+    }
+    await dynamoDb
+      .put({
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: data,
+      })
+      .promise();
+  }
+  list = async entity => {
+    const result = await dynamoDb.query({
+      TableName : process.env.DYNAMODB_TABLE,
+      KeyConditionExpression: "entity = :entity",
+      ExpressionAttributeValues: {
+        ":entity": entity,
+      }
+    }).promise()
+
+    return result
+  }
+  entity = entity => ({
+    get: id => this.get(entity, id),
+    put: data => this.put({ ...data, entity }),
+    list: () => this.list(entity)
+  })
+};
+
+const dynamoDB = table => new DynamoDB(table)
+
+export default dynamoDB;
